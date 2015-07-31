ca1abd551442b5ce0572f835fe5c0cc01aca90da
Michael Huang <mike.g.huang@gmail.com>
2015-07-31 04:30:33 -0700

    Clean up some imports

--- WCBNPlayer/src/main/java/org/wcbn/android/station/wcbn/WCBNStation.java
+++ WCBNPlayer/src/main/java/org/wcbn/android/station/wcbn/WCBNStation.java
@@ -16,12 +16,9 @@ import org.wcbn.android.Utils;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
 public class WCBNStation implements Station {
 
