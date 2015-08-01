c37e14098857fb4c43bab23ddd917d4161e0be82
Michael Huang <mike.g.huang@gmail.com>
2015-08-01 00:45:14 -0700

    Icon design prototype good
    Now need to generate assets for everything except xhdpi...

--- WCBNPlayer/src/main/java/org/wcbn/android/MainActivity.java
+++ WCBNPlayer/src/main/java/org/wcbn/android/MainActivity.java
@@ -25,7 +25,6 @@ import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.ShareActionProvider;
 
 import net.moraleboost.streamscraper.Stream;
 
